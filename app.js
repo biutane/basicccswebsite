8529e41a2627f206f45dae2fe4214fc8e0fd01d0
biutane <56445903+biutane@users.noreply.github.com>
2023-03-01 08:18:37 +0700

    3

--- app.js
+++ app.js
@@ -1,62 +1,62 @@
-const form = document.querySelector("#add-todo-form");
-form.addEventListener("submit", addList);
+// const form = document.querySelector("#add-todo-form");
+// form.addEventListener("submit", addList);
 
 const ref = firebase.database().ref("userList");
 
-function addList(event) {
-    event.preventDefault();
-    let title = document.getElementById("title").value;
-    const currentUser = firebase.auth().currentUser
-    ref.child(currentUser.uid).push({
-        title,
-    });
-    console.log("Add list complete!");
-    var toastContainer = document.querySelector(".toast-container");
-    toastContainer.innerHTML += `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                    <div class="toast-header">
-                    <img src="${currentUser.photoURL}" class="rounded me-2" alt="..." width='20px' height='20px'>
-                    <strong class="me-auto">${currentUser.displayName}</strong>
-                    <small class="text-muted">just now</small>
-                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    <div class="toast-body">
-                    ${title}
-                    </div>
-                </div>`
-    let toasts = toastContainer.querySelectorAll(".toast");
-    console.log(toasts);
-    let lastToast = toasts[toasts.length - 1]
-    var toast = new bootstrap.Toast(lastToast)
-    toast.show()
-    document.getElementById("title").value = "";
-    setTimeout(() => {
-        toast.hide()
-    }, 5000)
-}
+// function addList(event) {
+//     event.preventDefault();
+//     let title = document.getElementById("title").value;
+//     const currentUser = firebase.auth().currentUser
+//     ref.child(currentUser.uid).push({
+//         title,
+//     });
+//     console.log("Add list complete!");
+//     var toastContainer = document.querySelector(".toast-container");
+//     toastContainer.innerHTML += `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+//                     <div class="toast-header">
+//                     <img src="${currentUser.photoURL}" class="rounded me-2" alt="..." width='20px' height='20px'>
+//                     <strong class="me-auto">${currentUser.displayName}</strong>
+//                     <small class="text-muted">just now</small>
+//                     <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+//                     </div>
+//                     <div class="toast-body">
+//                     ${title}
+//                     </div>
+//                 </div>`
+//     let toasts = toastContainer.querySelectorAll(".toast");
+//     console.log(toasts);
+//     let lastToast = toasts[toasts.length - 1]
+//     var toast = new bootstrap.Toast(lastToast)
+//     toast.show()
+//     document.getElementById("title").value = "";
+//     setTimeout(() => {
+//         toast.hide()
+//     }, 5000)
+// }
 
-function readList(snapshot){
-    document.getElementById("name-list").innerHTML = "";
-    snapshot.forEach((data) => {
-        const id = data.key;
-        const title = data.val().title;
-        const newDiv = `<li id=${id} class='list-group-item row'><div class='col-11 d-inline-block'>${title}</div><div class='col-1 d-inline-block text-end'><button type="button" class="btn btn-outline-danger btn-delete" data-id="${id}"><i class="bi bi-trash3"></i></button></div></li>`;
-        document.getElementById("name-list").innerHTML += newDiv
-    })
-    document.querySelectorAll("button.btn-delete").forEach((btn) => {
-        btn.addEventListener("click", deleteList)
-    })
-}
+// function readList(snapshot){
+//     document.getElementById("name-list").innerHTML = "";
+//     snapshot.forEach((data) => {
+//         const id = data.key;
+//         const title = data.val().title;
+//         const newDiv = `<li id=${id} class='list-group-item row'><div class='col-11 d-inline-block'>${title}</div><div class='col-1 d-inline-block text-end'><button type="button" class="btn btn-outline-danger btn-delete" data-id="${id}"><i class="bi bi-trash3"></i></button></div></li>`;
+//         document.getElementById("name-list").innerHTML += newDiv
+//     })
+//     document.querySelectorAll("button.btn-delete").forEach((btn) => {
+//         btn.addEventListener("click", deleteList)
+//     })
+// }
 
 // ref.on("value", (data) => {
 //     readList(data)
 // })
 
-function deleteList(event){
-    const id = event.currentTarget.getAttribute('data-id');
-    const currentUser = firebase.auth().currentUser;
-    ref.child(currentUser.uid).child(id).remove();
-    console.log(`delete on id: ${id}`);
-}
+// function deleteList(event){
+//     const id = event.currentTarget.getAttribute('data-id');
+//     const currentUser = firebase.auth().currentUser;
+//     ref.child(currentUser.uid).child(id).remove();
+//     console.log(`delete on id: ${id}`);
+// }
 
 function getList(user) {
     if (user) {

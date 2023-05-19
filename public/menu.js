b0b3112269e2e37e89c66e4397d16387c861c33b
pmmiiiz <56476217+pmmiiiz@users.noreply.github.com>
2023-05-19 19:33:15 +0700

    menu

--- public/menu.js
+++ public/menu.js
@@ -34,3 +34,183 @@ function joinGame(){
         }
     })
 }
+
+var pone = document.querySelector('.howto1');
+var ptwo = document.querySelector('.howto2');
+var pthree = document.querySelector('.howto3');
+var pfour = document.querySelector('.howto4');
+var pfive = document.querySelector('.howto5');
+var btnone = document.querySelector('#page1')
+var btntwo = document.querySelector('#page2');
+var btnthree = document.querySelector('#page3');
+var btnfour = document.querySelector('#page4');
+var btnfive = document.querySelector('#page5');
+
+function toggle() {
+    var show = document.getElementById('howtoplay');
+    var bgact = document.getElementById('btnhowtoplay');
+    if(show.style.display == 'none'){
+        show.style.display = 'block'
+        bgact.style.backgroundColor = "#A234ED"
+        pone.style.display = 'block'
+        btnone.style.backgroundColor = '#515151'
+        ptwo.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+    else{
+        show.style.display = 'none';
+        bgact.style.backgroundColor = "#E473A0"
+        pone.style.display = 'block'
+        btnone.style.backgroundColor = '#515151'
+        ptwo.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+      
+}
+
+function pagetwo() {
+    if(ptwo.style.display == 'none'){
+        ptwo.style.display = 'block'
+        btntwo.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+    else{
+        ptwo.style.display = 'block'
+        btntwo.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+}
+
+function pagethree() {
+    if(pthree.style.display == 'none'){
+        pthree.style.display = 'block'
+        btnthree.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        ptwo.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+    else{
+        pthree.style.display = 'block'
+        btnthree.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        ptwo.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+}
+
+function pagefour() {
+    if(pfour.style.display == 'none'){
+        pfour.style.display = 'block'
+        btnfour.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        pthree.style.display = 'none'
+        ptwo.style.display = 'none'
+        pfive.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+    else{
+        pfour.style.display = 'block'
+        btnfour.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        pthree.style.display = 'none'
+        ptwo.style.display = 'none'
+        pfive.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+}
+
+function pagefive() {
+    if(pfive.style.display == 'none'){
+        pfive.style.display = 'block'
+        btnfive.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        ptwo.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btntwo.style.backgroundColor = '#DE4B92'
+    }
+    else{
+        pfive.style.display = 'block'
+        btnfive.style.backgroundColor = '#515151'
+        pone.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        ptwo.style.display = 'none'
+        btnone.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btntwo.style.backgroundColor = '#DE4B92'
+    }
+}
+
+function pageone() {
+    if(pone.style.display == 'none'){
+        pone.style.display = 'block'
+        btnone.style.backgroundColor = '#515151'
+        ptwo.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+    else{
+        pone.style.display = 'block'
+        btnone.style.backgroundColor = '#515151'
+        ptwo.style.display = 'none'
+        pthree.style.display = 'none'
+        pfour.style.display = 'none'
+        pfive.style.display = 'none'
+        btntwo.style.backgroundColor = '#DE4B92'
+        btnthree.style.backgroundColor = '#DE4B92'
+        btnfour.style.backgroundColor = '#DE4B92'
+        btnfive.style.backgroundColor = '#DE4B92'
+    }
+}

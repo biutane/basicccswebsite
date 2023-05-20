9750aa6404a613989eec3f9df3b8cdef240250e9
pmmiiiz <56476217+pmmiiiz@users.noreply.github.com>
2023-05-20 19:40:18 +0700

    copy

--- public/waiting.js
+++ public/waiting.js
@@ -197,3 +197,22 @@ btnBack.addEventListener("click", () => {
         }, 500)
     });
 })
+
+const answer = document.getElementById("copyResult");
+const copy   = document.getElementById("copyButton");
+const selection = window.getSelection();
+const range = document.createRange();
+const textToCopy = document.querySelector(".textToCopy")
+
+copy.addEventListener('click', function(e) {
+    range.selectNodeContents(textToCopy);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    const successful = document.execCommand('copy');
+    if(successful){
+      answer.innerHTML = 'Copied!';
+    } else {
+      answer.innerHTML = 'Unable to copy!';  
+    }
+    window.getSelection().removeAllRanges()
+});
